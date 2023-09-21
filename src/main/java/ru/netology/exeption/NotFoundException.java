4537eb5a21ac77a086e244a4330bf1771a10915e
elenalev2686 <elenalev2686@gmail.com>
2023-09-21 22:49:27 +0700

    changed src

--- src/main/java/ru/netology/exeption/NotFoundException.java
+++ src/main/java/ru/netology/exeption/NotFoundException.java
@@ -1,4 +1,5 @@
 package ru.netology.exeption;
+
 public class NotFoundException extends RuntimeException {
     public NotFoundException(int id) {
         super("Element with id: " + id + " not found");
